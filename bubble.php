13bffaa710e49b12769a29a4eb0c5b15fb2019a9
reitzd02 <2xzdenda2r@seznam.cz>
2023-01-11 08:30:08 +0100

    hgg

--- bubble.php
+++ bubble.php
@@ -4,7 +4,6 @@ function bubble_sort($arr) {
     for ($i = 0; $i < $n; $i++) {
       for ($j = 0; $j < $n - $i - 1; $j++) {
         if ($arr[$j] > $arr[$j + 1]) {
-          // Swap elements at indices $j and $j + 1
           $temp = $arr[$j];
           $arr[$j] = $arr[$j + 1];
           $arr[$j + 1] = $temp;
